4ba97f3c33ad92b59628eaffe57f2a947fbe1654
LinaCapera <38739985+LinaCapera@users.noreply.github.com>
2018-05-16 21:33:25 -0500

    coomit

--- src/Migrations/Version20180517015358.php
+++ src/Migrations/Version20180517015358.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180517012632 extends AbstractMigration
+final class Version20180517015358 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
